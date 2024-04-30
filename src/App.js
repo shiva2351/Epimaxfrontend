ebb15837782d51cc41107ec7b504779e69faab66
shiva <smpgr2351@gmail.com>
2024-04-30 19:36:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import {Component} from "react"
 import './App.css';
 
-function App() {
-  return (
+class  App extends Component {
+  state={todoUserInput:"",}
+  
+  textChange=(event)=>{
+    this.setState({todoUserInput:event.target.value})
+  }
+
+  onAddTodo=(event)=> {
+  
+    if (event.value === "") {
+      alert("Enter Valid Text");
+      return;
+    }
+  
+  
+    
+    
+  }
+
+  render(){
+    const {todoUserInput}=this.state
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class="todos-bg-container">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1 class="todos-heading">Todos</h1>
+            <h1 class="create-task-heading">
+              Create <span class="create-task-heading-subpart">Task</span>
+            </h1>
+            <input type="text" onChange={this.textChange} value={todoUserInput} class="todo-user-input" placeholder="What needs to be done?"/>
+            <button type='button' class="button" onClick={this.onAddTodo}>Add</button>
+            <h1 class="todo-items-heading">
+              My <span class="todo-items-heading-subpart">Tasks</span>
+            </h1>
+            <ul class="todo-items-container" id="todoItemsContainer"></ul>
+            <button class="button" id="saveTodoButton">Save</button>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    </div>
+  );}
 }
 
 export default App;
